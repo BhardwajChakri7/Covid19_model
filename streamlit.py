a3c5b36bbe0582e5dd183eba419a89c37962d17a
BhardwajChakri7 <155712824+BhardwajChakri7@users.noreply.github.com>
2024-10-18 17:26:36 +0530

    Create streamlit.py

---
+++ streamlit.py
@@ -0,0 +1,71 @@
+import pickle
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+
+# loading the saved models
+
+covid19 = pickle.load(open('BreastCancer_model.sav', 'rb'))
+# page title
+st.title('Covid 19 Prediction using ML')
+
+
+# getting the input data from the user
+col1, col2, col3,col4, col5 = st.columns(5)
+
+with col1:
+    Fever = st.text_input('Fever  ')
+    
+with col2:
+    Cough = st.text_input('Cough')
+
+with col3:
+    Shortness_of_Breath = st.text_input('Shortness_of_Breath')
+    
+with col4:
+    Fatigue = st.text_input('Fatigue')
+    
+with col5:
+    Loss_of_Taste_or_Smell = st.text_input('Loss_of_Taste_or_Smell')
+
+with col1:
+    Headache = st.text_input('Headache')
+    
+with col2:
+    Muscle_Pain = st.text_input('Muscle_Pain')
+
+with col3:
+    Diarrhea= st.text_input('Diarrhea')
+    
+with col4:
+    Nausea = st.text_input('Nausea')
+
+with col5:
+  Chest_Pain=st.text_input('Chest_Pain')
+
+with col1:
+    High_Blood_Pressure = st.text_input('High_Blood_Pressure')
+    
+with col2:
+    Diabetes = st.text_input('Diabetes')
+
+with col3:
+    Heart_Disease = st.text_input('Heart_Disease')
+    
+with col4:
+    Obesity = st.text_input('Obesity')
+
+# code for Prediction
+covid19_diagnosis = ''
+
+# creating a button for Prediction
+
+if st.button('Covid 19 Test Button'):
+    covid19_prediction = covid19.predict([[Fever,Cough,Shortness_of_Breath,Fatigue,Loss_of_Taste_or_Smell,Sore_Throat,Headache,Muscle_Pain,Diarrhea,Nausea,Chest_Pain,High_Blood_Pressure,Diabetes,Heart_Disease,Obesity]])
+    
+    if (covid19_prediction[0] == 1):
+      covid19_diagnosis = 'The person is effected by Covid 19'
+    else:
+      covid19_diagnosis = 'The person is not effected by Covid 19'
+    
+st.success(covid19_diagnosis)
